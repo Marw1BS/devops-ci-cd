cc6d53b121aa15843c788e6b7f6bfe2e608617ba
Marw1BS <marwan.benselma@gmail.com>
2025-06-23 16:32:32 +0200

    Test secret hello

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const secret = process.env.my_sec;
+
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+  res.json({ message: `Le secret est : ${secret}` });
 });
 
 app.listen(port, () => {
   console.log(`Backend listening on http://localhost:${port}`);
 });
-app.get('/', (req, res) => {
-  res.send('Backend root route: try /api');
-});
-
